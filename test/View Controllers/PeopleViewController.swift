8d42ff4bf3f828f2e043bdb020e4d9a004dfa206
martin vivanco <mvivancp@everis.com>
2021-01-18 12:09:03 -0500

    Update

---
+++ test/View Controllers/PeopleViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PeopleViewController.swift
+//  test
+//
+//  Created by everis on 1/18/21.
+//  Copyright © 2021 martin. All rights reserved.
+//
+
+import UIKit
+
+class PeopleViewController: UIViewController {
+    // MARK: - IBOutlet
+    // MARK: - Properties
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Personas"
+    }
+}
